d3c9e54ce31861f87e5ed6ce5e3b11a5b63a648f
Pau Penin <paupenin@gmail.com>
2023-04-01 13:59:05 +0200

    Create FileEditor component as a file editing placeholder

---
+++ src/components/editor/FileEditor/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+
+import hljs from 'highlight.js';
+
+import 'highlight.js/styles/github.css';
+import './styles.css';
+
+import codeExample from './codeExample';
+
+function FileEditor() {
+  useEffect(() => {
+    hljs.highlightAll();
+  });
+
+  return (
+    <div className="file-editor">
+      <div className="file-editor__header">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://test3.gno.land/p/demo/avl/avl.gno"
+        >
+          /p/demo/avl/avl.gno
+        </a>
+      </div>
+      <pre>
+        <code className="hljs language-go">
+          ${codeExample}
+        </code>
+      </pre>
+    </div>
+  );
+}
+
+export default FileEditor;
